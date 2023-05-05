adbe2f0c82566c2a83b879675805b0a487bee5d4
matheus.silva <tecmatheusg@hotmail.com>
2023-05-05 12:50:00 -0300

    first commit

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public static class Calculadora
+    {
+        public static int Somar(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int Subtrair(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        public static int Multiplicar(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        public static int Dividir(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
